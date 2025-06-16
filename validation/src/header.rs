a3f9845f81356b06844fbd6a1a3496894ed88b8c
Mathieu Ducroux <mathieu.ducroux@dfinity.org>
2025-06-16 09:15:19 +0200

    rustfmt

--- validation/src/header.rs
+++ validation/src/header.rs
@@ -76,7 +76,10 @@ pub fn validate_header(
         }
     };
 
-    if !matches!(network, BlockchainNetwork::Bitcoin(BitcoinNetwork::Testnet4)) {
+    if !matches!(
+        network,
+        BlockchainNetwork::Bitcoin(BitcoinNetwork::Testnet4)
+    ) {
         // Skip timestamp validation for Testnet4; the first 2 blocks are 2 days apart.
         // https://mempool.space/testnet4/block/00000000da84f2bafbbc53dee25a72ae507ff4914b867c565be350b0da8bf043
         is_timestamp_valid(store, header, current_time)?;
