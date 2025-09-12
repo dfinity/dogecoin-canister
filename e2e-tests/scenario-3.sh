31524d32e258229844898879a1b3d12329892bb6
Mathieu Ducroux <mathieu.ducroux@dfinity.org>
2025-09-12 15:26:49 +0200

    update e2e-tests

--- e2e-tests/scenario-3.sh
+++ e2e-tests/scenario-3.sh
@@ -21,7 +21,7 @@ dfx deploy --no-wallet dogecoin --argument "(record {
 
 # Send transaction valid transaction
 TX_BYTES="blob \"\\00\\00\\00\\00\\00\\01\\00\\00\\00\\00\\00\\00\""
-dfx canister call dogecoin bitcoin_send_transaction "(record {
+dfx canister call dogecoin dogecoin_send_transaction "(record {
   network = variant { regtest };
   transaction = ${TX_BYTES}
 })"
@@ -36,7 +36,7 @@ fi
 # Send invalid transaction.
 set +e
 TX_BYTES="blob \"12341234789789\""
-SEND_TX_OUTPUT=$(dfx canister call dogecoin bitcoin_send_transaction "(record {
+SEND_TX_OUTPUT=$(dfx canister call dogecoin dogecoin_send_transaction "(record {
   network = variant { regtest };
   transaction = ${TX_BYTES}
 })" 2>&1);
