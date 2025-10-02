d8b8561a064bd6cb4cf9622eed81e5a2205a8609
Mathieu Ducroux <mathieu.ducroux@dfinity.org>
2025-10-02 14:59:03 +0200

    Retrieve the block header without AuxPow information (first 80 bytes only)

--- bootstrap/3_compute_block_headers.sh
+++ bootstrap/3_compute_block_headers.sh
@@ -53,7 +53,8 @@ START_TIME=$(date +%s)
 echo "Fetching block headers up to height $STABLE_HEIGHT..."
 for ((height = 0; height <= STABLE_HEIGHT; height++)); do
     BLOCK_HASH=$("$DOGECOIN_CLI" -conf="$CONF_FILE" -datadir="$DATA_DIR" getblockhash "$height")
-    BLOCK_HEADER=$("$DOGECOIN_CLI" -conf="$CONF_FILE" -datadir="$DATA_DIR" getblockheader "$BLOCK_HASH" false)
+    # Retrieve the block header without AuxPow information (first 80 bytes only)
+    BLOCK_HEADER=$("$DOGECOIN_CLI" -conf="$CONF_FILE" -datadir="$DATA_DIR" getblockheader "$BLOCK_HASH" false | head -c160)
 
     # Append the block hash and header to the file.
     echo "$BLOCK_HASH,$BLOCK_HEADER" >> "$BLOCK_HEADERS_FILE"
