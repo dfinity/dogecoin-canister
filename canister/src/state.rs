dfinity/bitcoin-canister

refactor: validation of bitcoin headers and blocks (#419)
Unify validation of bitcoin headers and blocks to match the verification
done in the
[adapter](https://github.com/dfinity/ic/tree/88193c399d792854f98076e5311ffa12726f73e2/rs/bitcoin/adapter).
The goal is to have a single implementation for both components.

---------

Co-authored-by: Thomas Locher <thomas.locher@dfinity.org>

canister/src/state.rs
@@ -2,7 +2,7 @@ use crate::{
     address_utxoset::AddressUtxoSet,
     block_header_store::BlockHeaderStore,
     metrics::Metrics,
-    runtime::{inc_performance_counter, performance_counter, print, time_secs},
+    runtime::{duration_since_epoch, inc_performance_counter, performance_counter, print},
     types::{
         into_bitcoin_network, Address, BlockHeaderBlob, GetSuccessorsCompleteResponse,
         GetSuccessorsPartialResponse, Slicing,
@@ -15,7 +15,9 @@ use bitcoin::{block::Header, consensus::Decodable};
 use candid::Principal;
 use ic_btc_interface::{Fees, Flag, Height, MillisatoshiPerByte, Network};
 use ic_btc_types::{Block, BlockHash, OutPoint};
-use ic_btc_validation::{validate_header, ValidateHeaderError as InsertBlockError};
+use ic_btc_validation::{
+    BlockValidator, HeaderValidator, ValidateBlockError as InsertBlockError, ValidateHeaderError,
+};
 use serde::{Deserialize, Serialize};
 
 /// A structure used to maintain the entire state.
@@ -125,13 +127,12 @@ impl State {
 /// Returns an error if the block doesn't extend any known block in the state.
 pub fn insert_block(state: &mut State, block: Block) -> Result<(), InsertBlockError> {
     let start = performance_counter();
-    validate_header(
-        &into_bitcoin_network(state.network()),
-        &ValidationContext::new(state, block.header())
-            .map_err(|_| InsertBlockError::PrevHeaderNotFound)?,
-        block.header(),
-        time_secs(),
-    )?;
+    let validator = BlockValidator::new(
+        ValidationContext::new(state, block.header())
+            .map_err(|_| ValidateHeaderError::PrevHeaderNotFound)?,
+        into_bitcoin_network(state.network()),
+    );
+    validator.validate_block(block.internal_bitcoin_block(), duration_since_epoch())?;
 
     unstable_blocks::push(&mut state.unstable_blocks, &state.utxos, block)
         .expect("Inserting a block with a validated header must succeed.");
@@ -231,14 +232,13 @@ pub fn insert_next_block_headers(state: &mut State, next_block_headers: &[BlockH
 
         let validation_result =
             match ValidationContext::new_with_next_block_headers(state, &block_header)
-                .map_err(|_| InsertBlockError::PrevHeaderNotFound)
+                .map_err(|_| ValidateHeaderError::PrevHeaderNotFound)
             {
-                Ok(store) => validate_header(
-                    &into_bitcoin_network(state.network()),
-                    &store,
-                    &block_header,
-                    time_secs(),
-                ),
+                Ok(store) => {
+                    let validator =
+                        HeaderValidator::new(store, into_bitcoin_network(state.network()));
+                    validator.validate_header(&block_header, duration_since_epoch())
+                }
                 Err(err) => Err(err),
             };
 

canister/src/unstable_blocks/outpoints_cache.rs
@@ -253,6 +253,7 @@ mod test {
         let btc_network = into_bitcoin_network(network);
         let address_1 = random_p2pkh_address(btc_network).into();
         let address_2 = random_p2pkh_address(btc_network).into();
+        let address_3 = random_p2pkh_address(btc_network).into();
 
         let tx_0 = TransactionBuilder::coinbase()
             .with_output(&address_1, 1000)
@@ -289,11 +290,20 @@ mod test {
             .with_output(&address_2, 2000)
             .build();
 
+        let tx_coinbase_block_1 = TransactionBuilder::coinbase()
+            .with_output(&address_3, 10_000)
+            .build();
         let block_1 = BlockBuilder::with_prev_header(block_0.header())
+            .with_transaction(tx_coinbase_block_1.clone())
             .with_transaction(tx_1.clone())
             .build();
 
         cache.insert(&utxos, &block_1, 1).unwrap();
+
+        let outpoint_coinbase_1 = OutPoint {
+            txid: tx_coinbase_block_1.txid(),
+            vout: 0,
+        };
 
         let outpoint_1 = OutPoint {
             txid: tx_1.txid(),
@@ -310,6 +320,11 @@ mod test {
                         height: 0,
                         count: 2
                     },
+                    outpoint_coinbase_1.clone() => TxOutInfo {
+                        txout: (&tx_coinbase_block_1.output()[0]).into(),
+                        height: 1,
+                        count: 1
+                    },
                     outpoint_1.clone() => TxOutInfo {
                         txout: (&tx_1.output()[0]).into(),
                         height: 1,
@@ -321,7 +336,8 @@ mod test {
                         address_1.clone() => vec![OutPoint::new(tx_0.txid(), 0)]
                     },
                     block_1.block_hash() => maplit::btreemap! {
-                        address_2.clone() => vec![OutPoint::new(tx_1.txid(), 0)]
+                        address_2.clone() => vec![OutPoint::new(tx_1.txid(), 0)],
+                        address_3.clone() => vec![OutPoint::new(tx_coinbase_block_1.txid(), 0)]
                     },
                 },
                 removed_outpoints: maplit::btreemap! {
@@ -344,6 +360,11 @@ mod test {
                         height: 0,
                         count: 1
                     },
+                    outpoint_coinbase_1.clone() => TxOutInfo {
+                        txout: (&tx_coinbase_block_1.output()[0]).into(),
+                        height: 1,
+                        count: 1
+                    },
                     outpoint_1 => TxOutInfo {
                         txout: (&tx_1.output()[0]).into(),
                         height: 1,
@@ -352,7 +373,8 @@ mod test {
                 },
                 added_outpoints: maplit::btreemap! {
                     block_1.block_hash() => maplit::btreemap! {
-                        address_2 => vec![OutPoint::new(tx_1.txid(), 0)]
+                        address_2 => vec![OutPoint::new(tx_1.txid(), 0)],
+                        address_3.clone() => vec![OutPoint::new(tx_coinbase_block_1.txid(), 0)]
                     },
                 },
                 removed_outpoints: maplit::btreemap! {

dfinity/dogecoin-canister

canister/src/state.rs
@@ -2,7 +2,7 @@ use crate::{
     address_utxoset::AddressUtxoSet,
     block_header_store::BlockHeaderStore,
     metrics::Metrics,
-    runtime::{inc_performance_counter, performance_counter, print, time_secs},
+    runtime::{duration_since_epoch, inc_performance_counter, performance_counter, print},
     types::{
         into_dogecoin_network, Address, BlockHeaderBlob, GetSuccessorsCompleteResponse,
         GetSuccessorsPartialResponse, Slicing,
@@ -16,8 +16,7 @@ use candid::Principal;
 use ic_doge_interface::{Fees, Flag, Height, MillikoinuPerByte, Network};
 use ic_doge_types::{Block, BlockHash, OutPoint};
 use ic_doge_validation::{
-    AuxPowHeaderValidator, DogecoinHeaderValidator, ValidateHeaderError as InsertBlockError,
-    ValidateHeaderError,
+    BlockValidator, HeaderValidator, ValidateBlockError as InsertBlockError, ValidateHeaderError,
 };
 use serde::{Deserialize, Serialize};
 
@@ -128,13 +127,12 @@ impl State {
 /// Returns an error if the block doesn't extend any known block in the state.
 pub fn insert_block(state: &mut State, block: Block) -> Result<(), InsertBlockError> {
     let start = performance_counter();
-    let header_validator = DogecoinHeaderValidator::new(into_dogecoin_network(state.network()));
-    header_validator.validate_auxpow_header(
-        &ValidationContext::new(state, block.header())
+    let validator = BlockValidator::new(
+        ValidationContext::new(state, block.header())
             .map_err(|_| ValidateHeaderError::PrevHeaderNotFound)?,
-        block.auxpow_header(),
-        time_secs(),
-    )?;
+        into_dogecoin_network(state.network()),
+    );
+    validator.validate_block(block.internal_bitcoin_block(), duration_since_epoch())?;
 
     unstable_blocks::push(&mut state.unstable_blocks, &state.utxos, block)
         .expect("Inserting a block with a validated header must succeed.");
@@ -209,7 +207,6 @@ pub fn insert_next_block_headers(state: &mut State, next_block_headers: &[BlockH
     // Note that the actual limit available on system subnets is 50B. The threshold is set
     // lower to be conservative.
     const MAX_INSTRUCTIONS_THRESHOLD: u64 = 30_000_000_000;
-    let validator = DogecoinHeaderValidator::new(into_dogecoin_network(state.network()));
 
     for block_header_blob in next_block_headers.iter() {
         if inc_performance_counter() > MAX_INSTRUCTIONS_THRESHOLD {
@@ -237,7 +234,11 @@ pub fn insert_next_block_headers(state: &mut State, next_block_headers: &[BlockH
             match ValidationContext::new_with_next_block_headers(state, &block_header)
                 .map_err(|_| ValidateHeaderError::PrevHeaderNotFound)
             {
-                Ok(store) => validator.validate_auxpow_header(&store, &block_header, time_secs()),
+                Ok(store) => {
+                    let validator =
+                        HeaderValidator::new(store, into_dogecoin_network(state.network()));
+                    validator.validate_header(&block_header, duration_since_epoch())
+                }
                 Err(err) => Err(err),
             };
 

canister/src/unstable_blocks/outpoints_cache.rs
@@ -253,6 +253,7 @@ mod test {
         let doge_network = into_dogecoin_network(network);
         let address_1 = random_p2pkh_address(doge_network).into();
         let address_2 = random_p2pkh_address(doge_network).into();
+        let address_3 = random_p2pkh_address(doge_network).into();
 
         let tx_0 = TransactionBuilder::coinbase()
             .with_output(&address_1, 1000)
@@ -289,11 +290,20 @@ mod test {
             .with_output(&address_2, 2000)
             .build();
 
+        let tx_coinbase_block_1 = TransactionBuilder::coinbase()
+            .with_output(&address_3, 10_000)
+            .build();
         let block_1 = BlockBuilder::with_prev_header(block_0.header())
+            .with_transaction(tx_coinbase_block_1.clone())
             .with_transaction(tx_1.clone())
             .build();
 
         cache.insert(&utxos, &block_1, 1).unwrap();
+
+        let outpoint_coinbase_1 = OutPoint {
+            txid: tx_coinbase_block_1.txid(),
+            vout: 0,
+        };
 
         let outpoint_1 = OutPoint {
             txid: tx_1.txid(),
@@ -310,6 +320,11 @@ mod test {
                         height: 0,
                         count: 2
                     },
+                    outpoint_coinbase_1.clone() => TxOutInfo {
+                        txout: (&tx_coinbase_block_1.output()[0]).into(),
+                        height: 1,
+                        count: 1
+                    },
                     outpoint_1.clone() => TxOutInfo {
                         txout: (&tx_1.output()[0]).into(),
                         height: 1,
@@ -321,7 +336,8 @@ mod test {
                         address_1.clone() => vec![OutPoint::new(tx_0.txid(), 0)]
                     },
                     block_1.block_hash() => maplit::btreemap! {
-                        address_2.clone() => vec![OutPoint::new(tx_1.txid(), 0)]
+                        address_2.clone() => vec![OutPoint::new(tx_1.txid(), 0)],
+                        address_3.clone() => vec![OutPoint::new(tx_coinbase_block_1.txid(), 0)]
                     },
                 },
                 removed_outpoints: maplit::btreemap! {
@@ -344,6 +360,11 @@ mod test {
                         height: 0,
                         count: 1
                     },
+                    outpoint_coinbase_1.clone() => TxOutInfo {
+                        txout: (&tx_coinbase_block_1.output()[0]).into(),
+                        height: 1,
+                        count: 1
+                    },
                     outpoint_1 => TxOutInfo {
                         txout: (&tx_1.output()[0]).into(),
                         height: 1,
@@ -352,7 +373,8 @@ mod test {
                 },
                 added_outpoints: maplit::btreemap! {
                     block_1.block_hash() => maplit::btreemap! {
-                        address_2 => vec![OutPoint::new(tx_1.txid(), 0)]
+                        address_2 => vec![OutPoint::new(tx_1.txid(), 0)],
+                        address_3.clone() => vec![OutPoint::new(tx_coinbase_block_1.txid(), 0)]
                     },
                 },
                 removed_outpoints: maplit::btreemap! {