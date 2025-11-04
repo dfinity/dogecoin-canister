f087543b45f7c5b7b4bc3e5f30983fa82014994f
Mathieu Ducroux <mathieu.ducroux@dfinity.org>
2025-11-04 14:53:45 +0100

    update MillisatoshiPerByte to MillikoinuPerByte

--- examples/basic_dogecoin/src/service/get_current_fee_percentiles.rs
+++ examples/basic_dogecoin/src/service/get_current_fee_percentiles.rs
@@ -1,4 +1,4 @@
-use crate::{dogecoin_get_fee_percentiles, DOGE_CONTEXT};
+use crate::{dogecoin_get_fee_percentiles, MillikoinuPerByte, DOGE_CONTEXT};
 use ic_cdk::{
     bitcoin_canister::{
         GetCurrentFeePercentilesRequest, MillisatoshiPerByte,
@@ -6,10 +6,10 @@ use ic_cdk::{
     update,
 };
 
-/// Returns the 100 fee percentiles measured in millisatoshi/byte.
+/// Returns the 100 fee percentiles measured in millikoinu/byte.
 /// Percentiles are computed from the last 10,000 transactions (if available).
 #[update]
-pub async fn get_current_fee_percentiles() -> Vec<MillisatoshiPerByte> {
+pub async fn get_current_fee_percentiles() -> Vec<MillikoinuPerByte> {
     let ctx = DOGE_CONTEXT.with(|ctx| ctx.get());
 
     dogecoin_get_fee_percentiles(&GetCurrentFeePercentilesRequest {
