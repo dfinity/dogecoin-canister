42e4c39bdfff9a4514030887575ed0b2555a6845
Louis Pahlavi <louis.pahlavi@dfinity.org>
2025-11-28 14:48:49 +0100

    ci: add canister endpoint checks (#447)
    (DEFI-2498) Add WASM endpoints checks to CI pipeline for Bitcoin,
    watchdog and uploader canisters using `ic-wasm` `check-endpoints`.

--- e2e-tests/reproducibility.sh
+++ e2e-tests/reproducibility.sh
@@ -24,16 +24,16 @@ dockerfile_dir=$(dirname "$dockerfile")
 echo "Building Docker image (1st build)..."
 docker build -t canisters "$dockerfile_dir"
 
-# Create a temporary directory to store the wasm files
-tmpdir=$(mktemp -d)
+# Create a directory to store the wasm files
+mkdir -p wasms
 
 # Extract the wasm files from the first build
-docker run --rm -v "$tmpdir:/output" canisters cp /uploader.wasm.gz /output/uploader.wasm.gz
-docker run --rm -v "$tmpdir:/output" canisters cp /ic-doge-canister.wasm.gz /output/ic-doge-canister.wasm.gz
+docker run --rm -v "./wasms:/wasms" canisters cp /uploader.wasm.gz /wasms/uploader.wasm.gz
+docker run --rm -v "./wasms:/wasms" canisters cp /ic-doge-canister.wasm.gz /wasms/ic-doge-canister.wasm.gz
 
 # Calculate the SHA256 sums for the first build
 echo "Calculating SHA256 sums (1st build)..."
-if ! sha256sum1=$(sha256sum "$tmpdir/uploader.wasm.gz" "$tmpdir/ic-doge-canister.wasm.gz" 2>&1); then
+if ! sha256sum1=$(sha256sum "wasms/uploader.wasm.gz" "wasms/ic-doge-canister.wasm.gz" 2>&1); then
   echo "ERROR: Failed to calculate SHA256 sums for 1st build"
   echo "$sha256sum1"
   exit 1
@@ -44,12 +44,12 @@ echo "Building Docker image (2nd build)..."
 docker build -t canisters "$dockerfile_dir"
 
 # Extract the wasm files from the second build
-docker run --rm -v "$tmpdir:/output" canisters cp /uploader.wasm.gz /output/uploader.wasm.gz
-docker run --rm -v "$tmpdir:/output" canisters cp /ic-doge-canister.wasm.gz /output/ic-doge-canister.wasm.gz
+docker run --rm -v "./wasms:/wasms" canisters cp /uploader.wasm.gz /wasms/uploader.wasm.gz
+docker run --rm -v "./wasms:/wasms" canisters cp /ic-doge-canister.wasm.gz /wasms/ic-doge-canister.wasm.gz
 
 # Calculate the SHA256 sums for the second build
 echo "Calculating SHA256 sums (2nd build)..."
-if ! sha256sum2=$(sha256sum "$tmpdir/uploader.wasm.gz" "$tmpdir/ic-doge-canister.wasm.gz" 2>&1); then
+if ! sha256sum2=$(sha256sum "wasms/uploader.wasm.gz" "wasms/ic-doge-canister.wasm.gz" 2>&1); then
   echo "ERROR: Failed to calculate SHA256 sums for 2nd build"
   echo "$sha256sum2"
   exit 1
