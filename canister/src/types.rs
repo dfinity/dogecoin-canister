dfinity/bitcoin-canister

feat: add network validation for addresses in get_balance and get_utxos requests (#458)
[DEFI-2519](https://dfinity.atlassian.net/browse/DEFI-2519?atlOrigin=eyJpIjoiNDc5ODg4MDk2YWViNGE1YWE3MGIxZWYxOTRlMGVjNjMiLCJwIjoiaiJ9):
When `bitcoin_get_balance` and `bitcoin_get_utxos` are called and the
address does not match a known format (e.g. P2PKH, etc.), the canister
returns a `MalformedAddress` error. However, no such error is returned
when the address is an address for the wrong network (e.g. testnet
instead of mainnet). This can be confusing for users trying to get the
balance of e.g. a testnet address from the mainnet canister and getting
0 as balance. This PR adds the `from_str_checked` which is used in
`get_balance` and `get_utxos` to validate that the address in the
request belongs to the same network as the canister called.

[DEFI-2519]:
https://dfinity.atlassian.net/browse/DEFI-2519?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

canister/src/types.rs
@@ -457,6 +457,15 @@ pub struct GetSuccessorsPartialResponse {
 #[derive(Debug, PartialEq, Eq)]
 pub struct InvalidAddress;
 
+/// Error type for address parsing with network validation.
+#[derive(Debug, PartialEq, Eq)]
+pub enum AddressParseError {
+    /// The address string is malformed and cannot be parsed.
+    MalformedAddress,
+    /// The address is valid but belongs to a different network.
+    WrongNetwork { expected: Network },
+}
+
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize, Eq, Ord, PartialOrd)]
 pub struct Address(String);
 
@@ -467,21 +476,26 @@ impl Address {
             .map(|address| Self(address.to_string()))
             .map_err(|_| InvalidAddress)
     }
+
+    /// Parses an address string and validates it belongs to the expected network.
+    pub fn from_str_checked(s: &str, expected_network: Network) -> Result<Self, AddressParseError> {
+        let unchecked_address =
+            BitcoinAddress::from_str(s).map_err(|_| AddressParseError::MalformedAddress)?;
+
+        let bitcoin_network = into_bitcoin_network(expected_network);
+
+        unchecked_address
+            .require_network(bitcoin_network)
+            .map(|address| Address(address.to_string()))
+            .map_err(|_| AddressParseError::WrongNetwork {
+                expected: expected_network,
+            })
+    }
 }
 
 impl From<BitcoinAddress> for Address {
     fn from(address: BitcoinAddress) -> Self {
         Self(address.to_string())
-    }
-}
-
-impl FromStr for Address {
-    type Err = InvalidAddress;
-
-    fn from_str(s: &str) -> Result<Self, InvalidAddress> {
-        BitcoinAddress::from_str(s)
-            .map(|address| Address(address.assume_checked().to_string()))
-            .map_err(|_| InvalidAddress)
     }
 }
 

dfinity/dogecoin-canister

canister/src/types.rs
@@ -448,6 +448,15 @@ pub struct GetSuccessorsPartialResponse {
 #[derive(Debug, PartialEq, Eq)]
 pub struct InvalidAddress;
 
+/// Error type for address parsing with network validation.
+#[derive(Debug, PartialEq, Eq)]
+pub enum AddressParseError {
+    /// The address string is malformed and cannot be parsed.
+    MalformedAddress,
+    /// The address is valid but belongs to a different network.
+    WrongNetwork { expected: Network },
+}
+
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize, Eq, Ord, PartialOrd)]
 pub struct Address(String);
 
@@ -458,21 +467,26 @@ impl Address {
             .map(|address| Self(address.to_string()))
             .map_err(|_| InvalidAddress)
     }
+
+    /// Parses an address string and validates it belongs to the expected network.
+    pub fn from_str_checked(s: &str, expected_network: Network) -> Result<Self, AddressParseError> {
+        let unchecked_address =
+            DogecoinAddress::from_str(s).map_err(|_| AddressParseError::MalformedAddress)?;
+
+        let dogecoin_network = into_dogecoin_network(expected_network);
+
+        unchecked_address
+            .require_network(dogecoin_network)
+            .map(|address| Address(address.to_string()))
+            .map_err(|_| AddressParseError::WrongNetwork {
+                expected: expected_network,
+            })
+    }
 }
 
 impl From<DogecoinAddress> for Address {
     fn from(address: DogecoinAddress) -> Self {
         Self(address.to_string())
-    }
-}
-
-impl FromStr for Address {
-    type Err = InvalidAddress;
-
-    fn from_str(s: &str) -> Result<Self, InvalidAddress> {
-        DogecoinAddress::from_str(s)
-            .map(|address| Address(address.assume_checked().to_string()))
-            .map_err(|_| InvalidAddress)
     }
 }
 